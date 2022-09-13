b0d2057a3b27c13a5d63cee1c913b2c9c3a25f23
GitHub Action <action@github.com>
2022-09-13 11:33:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/vvboy/quicksearchfunctionforphpstorm/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/vvboy/quicksearchfunctionforphpstorm/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.vvboy.quicksearchfunctionforphpstorm.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.vvboy.quicksearchfunctionforphpstorm.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/vvboy/quicksearchfunctionforphpstorm/services/MyProjectService.kt
+++ src/main/kotlin/com/github/vvboy/quicksearchfunctionforphpstorm/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.vvboy.quicksearchfunctionforphpstorm.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.vvboy.quicksearchfunctionforphpstorm.MyBundle
 
 class MyProjectService(project: Project) {
 
